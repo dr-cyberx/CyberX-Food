d8760cd08b0c140597f537e39f9afe18375b793f
Cd-Vishal007 <vishal.codedrill@gmail.com>
2021-08-07 16:29:37 +0530

    adding the default results

--- src/screen/SearchScreen.jsx
+++ src/screen/SearchScreen.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Text, View, StyleSheet } from "react-native";
 import SearchBar from "../components/SearchBar";
 import yelp from "../api/yelp";
@@ -6,21 +6,30 @@ import yelp from "../api/yelp";
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
   const [result, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
 
   const searchApi = async () => {
-    const response = await yelp.get("/search", {
-      params: {
-        limits: 50,
-        term,
-        location: "san jose",
-      },
-    });
-    setResults(response.data.businesses);
+    try {
+      const response = await yelp.get("/search", {
+        params: {
+          limits: 50,
+          term,
+          location: "san jose",
+        },
+      });
+      setResults(response.data.businesses);
+    } catch (err) {
+      setErrorMessage("Oops! Something went wrong");
+    }
   };
+
+  useEffect(() => {
+    searchApi('pasta')
+  }, []);
 
   return (
     <View style={style.SearchScreen}>
-      <Text>{term}</Text>
+      <Text>{errorMessage}</Text>
       <SearchBar
         term={term}
         onTermChange={(newTerm) => setTerm(newTerm)}
